b459038e8f91e63e1534e90ed4e1c42cb120cc56
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-06-25 10:00:42 +0545

    Update App.js

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -25,10 +25,18 @@ function App() {
         <span>{('0'+ Math.floor(time % 100)).slice(-2)}</span>
       </div>
       <div>
-        <button onClick={() => setTimer(true)}>Start</button>
-        <button onClick={() => setTimer(false)}>Stop</button>
-        <button onClick={() => setTimer(true)}>Resume</button>
-        <button onClick={() => setTime(0)}>Reset</button>
+        {!timeon && time === 0 &&(
+          <button onClick={() => setTimer(true)}>Start</button>
+        )}
+        {timeon && (
+          <button onClick={() => setTimer(false)}>Stop</button>
+        )}
+        {!timeon && time !== 0 &&(
+          <button onClick={() => setTimer(true)}>Resume</button>
+        )}
+        {!timeon && time !== 0 &&(
+          <button onClick={() => setTime(0)}>Reset</button>
+        )}  
       </div>
     </div>
   );
