dce1b8c00acd323453cc385a06e00e51afcf1e70
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-06-25 09:32:54 +0545

    Adding html and javascript

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
 function App() {
+  const [time, setTime] = React.useState(0)
+  const [timeon,setTimer] = React.useState(false)
+
+  React.useEffect(() => {
+    let interval = null;
+    if (timeon){
+      interval = setInterval(() => {
+        setTime(time => time + 1)
+      },10 )
+    }else{
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+  }, [timeon])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span>{('0'+ Math.floor((time / 6000) % 60)).slice(-2)}:</span>
+        <span>{('0'+ Math.floor((time / 100) % 60)).slice(-2)}:</span>
+        <span>{('0'+ Math.floor(time % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        <button onClick={() => setTimer(true)}>Start</button>
+        <button onClick={() => setTimer(false)}>Stop</button>
+        <button onClick={() => setTimer(true)}>Resume</button>
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
     </div>
   );
 }
